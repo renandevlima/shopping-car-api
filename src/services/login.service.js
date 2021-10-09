7b02493badde40ccaaf5e28c104d09fcc67d3c84
Renan Lima <renan.arantes123@gmail.com>
2021-10-09 12:52:57 -0300

    removido nodemodules do git e criado login

---
+++ src/services/login.service.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const db = [{username : "admin", password : "admin"}];
+
+const authenticate = async ({username, password}) => {
+    const user = db.find((user) => (
+        user.password === password && 
+        user.username === username 
+    ));
+
+    if(user) {
+        const token = await jwt.sign({username}, 'configuracaodasuaaplicacao');
+
+        return {
+            token,
+            user
+        }
+    }
+};
+
+module.exports = {
+    authenticate
+};
